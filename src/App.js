bb1c04187ccef21d5c802a89c95f219403e4afe5
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-07-05 11:03:28 -0500

    NewsApi con React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Formulario from './components/Formulario'
+import Header from './components/Header'
+import ListadoNoticias from './components/ListadoNoticias';
 
-function App() {
+const App = () => {
+  //definir la categoria y noticias
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, setNoticias] = useState([]);
+  // 
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=9634f5b79d9848eab8ec8cd1d6e4b4b0`;
+
+      const respuesta = await fetch(url)
+
+      const noticias = await respuesta.json();
+      setNoticias(noticias.articles)
+    }
+    consultarApi();
+  }, [categoria]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header titulo="Buscador de Noticias" />
+      <div className="container-white">
+        <Formulario guardarCategoria={guardarCategoria} />
+
+        <ListadoNoticias noticias={noticias} />
+
+      </div>
+    </>
+  )
 }
 
-export default App;
+
+export default App
+
